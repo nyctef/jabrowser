f131730902a811ba6741660be66701966768ed2f
Nyctef <nyctef@nyctef.com>
2016-08-27 17:47:33 +0100

    pulling info out of muc stanzas and rethrowing more specific events

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+var EventEmitter = require('events')
 var XmppClient = require('node-xmpp-client')
 var config = require('./config')
+var JID = require('node-xmpp-jid')
 
 var mucxmlns = 'http://jabber.org/protocol/muc'
+var log = console.log
+var mucEvents = new EventEmitter()
 
 var client = new XmppClient({
   jid: config.jid,
   password: config.password,
 })
-
-var log = console.log
 
 client.on('online', function(data) {
   var jid = data.jid
@@ -17,7 +19,22 @@ client.on('online', function(data) {
 })
 
 client.on('stanza', function(stanza) {
-  log('incoming stanza: ', stanza.toString())
+  if (stanza.is('presence')) {
+    handlePresence(stanza)
+    return
+  }
+  if (stanza.is('message')) {
+    switch(stanza.type) {
+      case 'groupchat': 
+        handleGroupchat(stanza)
+        break
+      default:
+        log('unrecognised message stanza: ', stanza.toString())
+        log(stanza)
+        break
+    }
+    return
+  }
 })
 
 client.on('error', function (err) {
@@ -28,7 +45,7 @@ client.on('error', function (err) {
 function joinRoom(client, jid, room, roomNick) {
   log('joining room ' + room + ' with nick ' + roomNick)
   var stanza = joinRoomStanza(jid, room, roomNick)
-  log(stanza)
+  //log(stanza)
   client.send(stanza)
 }
 
@@ -38,3 +55,43 @@ function joinRoomStanza(jid, room, roomNick) {
     to: `${room}/${roomNick}`
   }).c('x', {xmlns: mucxmlns})
 }
+
+function handleGroupchat(stanza) {
+  if (stanza.getChild('subject')) {
+    mucEvents.emit('topic', {
+      topic: stanza.getChild('subject').text()
+    })
+    return
+  }
+
+  if (stanza.getChild('delay')) {
+    mucEvents.emit('delayed_message', {
+      speaker: JID(stanza.from).resource,
+      message: stanza.getChild('body').text(),
+      timestamp: stanza.getChild('delay').attrs.stamp,
+    })
+    return
+  }
+
+  mucEvents.emit('incoming_message', {
+    speaker: JID(stanza.from).resource,
+    message: stanza.getChild('body').text(),
+    timestamp: new Date().toISOString(),
+  })
+}
+
+function handlePresence(stanza) {
+  // TODO later
+}
+
+mucEvents.on('topic', function(topic) {
+  log('topic', topic)
+})
+
+mucEvents.on('delayed_message', function(message) {
+  log('delayed_message', message)
+})
+
+mucEvents.on('incoming_message', function(message) {
+  log('incoming_message', message)
+})
