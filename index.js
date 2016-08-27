63c9182a3f852218ef21b9c2c64992c7cc97ea26
Nyctef <nyctef@nyctef.com>
2016-08-27 18:19:47 +0100

    start sending websocket events to a browser client

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//
+// connecting to xmpp and rasing some events
+//
+
 var EventEmitter = require('events')
 var XmppClient = require('node-xmpp-client')
 var config = require('./config')
@@ -95,3 +99,46 @@ mucEvents.on('delayed_message', function(message) {
 mucEvents.on('incoming_message', function(message) {
   log('incoming_message', message)
 })
+
+//
+// webserver and websockets
+//
+
+var Primus = require('primus')
+var http = require('http')
+var fs = require('fs')
+var server = http.createServer(function(req, res) {
+  if (req.url == '/') {
+    fs.readFile('./index.html', function(err, content) {
+      if (err) {
+        console.error(error)
+        res.writeHead(500)
+        res.end(JSON.stringify(error))
+      } else {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.end(content, 'utf-8')
+      }
+    })
+  }
+})
+
+server.listen(3333)
+log('server is listening on :3333')
+var primus = new Primus(server, {
+  transformer: 'engine.io',
+})
+
+primus.on('connection', function(spark) {
+  log(`we got a connection from ${spark.address} called ${spark.id}`)
+  mucEvents.on('incoming_message', function(message) {
+    log(`sending incoming_message to ${spark.id}`)
+    spark.write({type: 'incoming_message', message: message})
+  })
+})
+
+primus.on('disconnection', function(spark) {
+  log(`we got a disconnection from ${spark.address} called ${spark.id}`)
+})
+
+
+
